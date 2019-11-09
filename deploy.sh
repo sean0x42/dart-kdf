5bfbadb23f2c32861f3e867870aafc6b1dc5f4f8
Sean Bailey <sean@seanbailey.io>
2019-11-09 19:32:05 +1100

    Use mdbook in Zeit Now config

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
-mdbook build spec
-now spec/book --prod --name kdf
+yum install -y wget
+
+wget https://github.com/rust-lang/mdBook/releases/download/v0.3.4/mdbook-v0.3.4-x86_64-unknown-linux-gnu.tar.gz
+
+tar -xzf mdbook-v0.3.4-x86_64-unknown-linux-gnu.tar.gz
+
+./mdbook build spec
